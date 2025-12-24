03f83ee522f242fcf831023462274078d40eeba5
anatolij1923 <kostyahaustow@gmail.com>
2025-12-24 14:26:55 +0300

    chore: cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,4 @@
 export default function App() {
-  const posts = [
-    {
-      name: "Post about shitting in bed",
-      desc: "Never shittin where im sleepin",
-    },
-    {
-      name: "Post 2",
-      desc: "blah blah blah",
-    },
-    {
-      name: "Post 3",
-      desc: "gdsjlgj ljgl jsdklgj sjg kldsjl",
-    },
-    {
-      name: "Post about someting",
-      desc: "Never fgkljdfklgjdfskl",
-    },
-  ];
-
   return (
     <div>
       <h1>pets</h1>
