e1564b5351fe360fbcdd7dc1d03d4d7940942e09
anatolij1923 <kostyahaustow@gmail.com>
2025-12-24 14:24:46 +0300

    feat: added tRPC on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,23 @@
-console.log("wassup");
+import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { trpcRouter } from "./trpc.js";
+
+const expressApp = express();
+expressApp.get("/ping", (req, res) => {
+  res.send("IDI HAXYU");
+});
+
+// expressApp.get("/posts", (req, res) => {
+//   res.send(posts);
+// });
+
+expressApp.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  }),
+);
+
+expressApp.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
