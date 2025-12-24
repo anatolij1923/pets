68c2b774c6c4a05a8a267402015f7b94d4e4ef45
anatolij1923 <kostyahaustow@gmail.com>
2025-12-24 13:02:27 +0300

    chore: cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const posts = [
+    {
+      name: "Post about shitting in bed",
+      desc: "Never shittin where im sleepin",
+    },
+    {
+      name: "Post 2",
+      desc: "blah blah blah",
+    },
+    {
+      name: "Post 3",
+      desc: "gdsjlgj ljgl jsdklgj sjg kldsjl",
+    },
+    {
+      name: "Post about someting",
+      desc: "Never fgkljdfklgjdfskl",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>pets</h1>
+      {posts.map((post) => {
+        return (
+          <div>
+            <h2>{post.name}</h2>
+            <p>{post.desc}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
-
-export default App;
